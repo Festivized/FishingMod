b63a4bfa8b77fff00674066db07223d893f2d9ad
Festivized <1zhangjoe@gmail.com>
2025-04-15 13:46:58 -0400

    25/04/15 I have no idea what im doing, what the fuck is this shit

--- src/main/java/festive/fishingMod/mymod.java
+++ src/main/java/festive/fishingMod/mymod.java
@@ -1,4 +1,4 @@
-package festive.FishingMod;
+package festive.fishingMod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,8 +29,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod {
+@Mod(mymod.MODID)
+public class mymod {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
@@ -75,7 +75,7 @@ public class ExampleMod {
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context) {
+    public mymod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
         // Register the commonSetup method for modloading
